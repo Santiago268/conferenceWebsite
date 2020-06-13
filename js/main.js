6008f340f364835b9a8574b53169f30eb8a54942
Santiago Martín Cejas <santi_martin_cejas@hotmail.com>
2020-06-13 05:09:34 +0200

    Updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(){
   "use strict";
 
+  var regalo= document.getElementById('regalo');
   document.addEventListener('DOMContentLoaded', function(){
     console.log("listo");
 
@@ -12,13 +13,33 @@
     // Campos para los tickets
     var pase_dia = document.getElementById('pase-dia');
     var pase_completo = document.getElementById('pase-completo');
-    var pase-dosdias = document.getElementById('pase-dosdias');
+    var pase_dosdias = document.getElementById('pase-dosdias');
 
     // Botones & Div
     var calcular = document.getElementById('calcular');
     var errorDiv = document.getElementById('error');
     var botonRegistro = document.getElementById('btnRegistro');
-    var resultado = documente.getElementById('lista-productos');
+    var resultado = document.getElementById('lista-productos');
+
+    calcular.addEventListener('click', calcularTotal);
+
+
+
+    function calcularTotal(event){
+      event.preventDefault();
+      if(regalo.value == ''){
+        alert("You must choose a Gift!!");
+        regalo.focus();
+      }else{
+        var ticketDia= pase_dia.value,
+            ticket2Dias= pase_dosdias.value,
+            ticketCompleto = pase_completo.value;
+
+        var costeTickets = (ticketDia * 30) + (ticket2Dias * 45) + (ticketCompleto * 50);
+        console.log(costeTickets);
+
+      }
+    }
 
   }); //DOM CONTENT LOADED
 
