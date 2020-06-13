b36cfa12687f43196a7053e1466ef20bd286c2e2
Santiago Martín Cejas <santi_martin_cejas@hotmail.com>
2020-06-13 04:42:43 +0200

    Updated

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@
 
   document.addEventListener('DOMContentLoaded', function(){
     console.log("listo");
+
+    // Campos datos de usuario
+    var nombre = document.getElementById('nombre');
+    var apellido = document.getElementById('apellido');
+    var email = document.getElementById('email');
+
+    // Campos para los tickets
+    var pase_dia = document.getElementById('pase-dia');
+    var pase_completo = document.getElementById('pase-completo');
+    var pase-dosdias = document.getElementById('pase-dosdias');
+
+    // Botones & Div
+    var calcular = document.getElementById('calcular');
+    var errorDiv = document.getElementById('error');
+    var botonRegistro = document.getElementById('btnRegistro');
+    var resultado = documente.getElementById('lista-productos');
+
   }); //DOM CONTENT LOADED
 
 })();
