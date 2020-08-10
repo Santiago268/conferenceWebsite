b41ac4421b8a359cb69772548f966dbb63d56d65
Santiago Martín Cejas <santi_martin_cejas@hotmail.com>
2020-08-10 11:10:47 +0200

    Php introduction

---
+++ includes/templates/header.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/all.css">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Oswald:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+  <header class="site-header">
+    <div class="hero">
+      <div class="contenido-header">
+        <nav class="redes-sociales">
+          <a href="#"><i class="fab fa-facebook-square"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-pinterest"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </nav>
+        <div class="informacion-evento">
+          <div class="informacion-evento-contenido">
+            <p class="fecha"><i class="fas fa-calendar-alt"></i> 25-26 August</p>
+            <p class="ciudad"><i class="fas fa-map-marker-alt"></i> Malaga, Spain</p>
+          </div>
+          <h1 class="nombre-sitio">Malaga Software Conference</h1>
+          <p class="slogan">The best conference in <span>software development</span></p>
+        </div>
+      </div>
+    </div><!--.hero-->
+  </header>
+  <div class="barra">
+    <div class="contenedor">
+      <div class="navegacion">
+        <div class="logotipo">
+          <a href="index.html">
+            <p><i class="fas fa-angle-right"></i>Malaga Soft. Conf.</p>
+          </a>
+        </div>
+        
+        
+        <div class="menu-mobile">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <nav class="menu-navegacion">
+              <a href="conferencia.php">Conference</a>
+              <a href="#">Schedule</a>
+              <a href="#">Guests</a>
+              <a href="registro.html">Booking</a>
+        </nav>
+      </div>
+    </div><!--.contenedor-->
+  </div><!--barra-->
